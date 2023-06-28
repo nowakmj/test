deef6d5f181be3d7248b2138bf5ce2fa87df5d44
Mateusz Nowak <mateusz.nowak1501@gmail.com>
2023-06-28 11:09:30 +0200

    Revert "poprawka"
    This reverts commit d365d057f86e50e553dc4586024f854422f13357.

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -91,6 +91,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     *
+     * @return string Username
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
      * Getter for roles.
      *
      * @return array<int, string> Roles
@@ -156,10 +166,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
-    }
-
-    public function getUsername()
-    {
-        // TODO: Implement getUsername() method.
     }
 }
